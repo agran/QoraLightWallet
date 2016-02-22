b8375a5e0ba6d1bd67258a61152f80ecd1d80de6
agran <agran@agran.net>
2016-02-22 07:56:10 +0300

    Added menu in a separate file

---
+++ menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+	$("#menu").html(
+	  '<a href="http://qora.co.in/light"><img height=107 width=93 src="qora.png"></a>'
+	+ '<br><br><b>'
+	+ '<a href=brainwallet.html>BrainWallet</a> | '
+	+ '<a href=payment.html>Payment Processor</a> | '
+	+ '<a href=http://mirror.qora.co.in:9090/index/blockexplorer.html>BlockExplorer</a>'
+	+ '</b><br><br>');
+	
+	$("#bottom").html('(c) <a href="mailto:agran@agran.net">agran</a><br>QTz6fSV2VNc2wjwwsw57kwQzgQhmGw5idQ');
+
+});
